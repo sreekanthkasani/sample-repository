c009d53f86f0bcf9fc81461a84bae130821a5dc7
sreekanthkasani <sreekanthkasani22@gmail.com>
2018-08-09 18:32:38 +0530

    i love you

---
+++ jz4u.bat
@@ -0,0 +1,66 @@
+@echo off
+setlocal EnableDelayedExpansion
+mode con: cols=300 lines=100
+color 0a
+
+ echo. 
+ echo.
+ echo.
+ echo.
+ echo.
+ echo.
+ echo.
+ echo.
+                                                                                                 
+ echo.                                                                  .ymmmmdddmNmmmdddm+                     
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                                 `dNNmmddmmmmhyhddddmh/`                  
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                                 hNmmddhddddysooyhhdmmmh                  
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                                /Nmddddddhyo+///++osydmm`                 
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                                +mmmddmdhso+////+++oohNm/                 
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                                ommmmmmdyo+/////++++oshy.                 
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                               .hmmmmmmho+///+oo+++oyhh-                  
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                               yddddmdyo++oyhhhyo++hmhs                   
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                              /ddhhdds/:/+syhsyo/::syys`                  
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                             /dmmsosho::---:///:---/o++.                  
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                             sddmds/oo//:----:++:::/ooo                   
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                            .ddmmdmshy+///://++//+ssyo`                   
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                            +dmmmmNhhh+++//++so++osso`                    
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                            ydmNmNNmmm+/+oo++///+oso`                     
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                           .dmNNNNmmmmo::++oso++oyhs`.                    
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                          `+yyo+ohmmddy+:/++oshdmmmyyso/-:/:.             
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                       `.:-/o/:/::ohhddds/+/hdmmdy++++/+/--/o-            
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                     `::/-::+oo/:/--/:/+oo:/+/-h+:/o///-+yyo-/./.         
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                    ./-/:::--::/+oo/::/:-o+:--+h///--/oo-.-:.+.-/.`       
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                    .///-+sosyys+/://+o+//h+//ys:/oos+:-oyho../-./-`      
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                    --+/+o/---://+ooo+/:++yo--ho/:-..:+s:o+./:-::--:.`    
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                    :-+o:ssdhsys+/:--:+ossydsod--:osyyysos+/:-//.-/:.-    
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                    -/s/+yyyooyhhhhhhso+:/sd+odyhyyhsss/oos+:/.:+-.:/-    
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                    :s++yyddyooo+oosshdhdh/ydddyo+o+++/yohhs//+.-+/:.-    
+ ping 127.0.0.1 -n 1 > nul
+ echo                                                    +/:ssyddysys+o//+/:ooo-/hhy://:/+://syyo/--:-.-::-    
+ ping 127.0.0.1 -n 1 > nul
+
+pause > nul
